e2bdf4a23c29d53d5c100d503bdb034db25d27f9
radekBednarik <radek.bednarik@tesena.com>
2024-04-08 16:23:56 +0200

    dev: weird

--- tests/specs/test.spec.ts
+++ tests/specs/test.spec.ts
@@ -3,10 +3,15 @@ import { driver, expect } from "@wdio/globals";
 describe("Settings Screen Test", () => {
   it("should open the Settings screen", async () => {
     // Example: Locating and clicking on the settings button
+
+    // Example: Checking if the Settings screen is displayed
+    await driver.execute("mobile: shell", {
+      command: "am start -n com.android.settings/.Settings",
+    });
+
     const settingsButton = await driver.$('//[@id="settings-button"]');
     await settingsButton.click();
 
-    // Example: Checking if the Settings screen is displayed
     const settingsScreen = await driver.$('//[@id="settings-screen"]');
     const isDisplayed = await settingsScreen.isDisplayed();
     expect(isDisplayed).toBe(true);
